fab7ace214e760fbeed5f65292d89ebd443da9ef
Alecsander Camilo <alecsandercamilo@gmail.com>
2022-07-18 17:54:27 -0300

    Challenge

---
+++ test/pageObjects/fileAClaim.page.js
@@ -0,0 +1,54 @@
+import BasePage from "./base.page";
+
+class FileAClaimPage extends BasePage {
+    open() { return super.open("/claim-form") }
+
+    // Mandatory Fields
+    get inputName() { 
+        return $('#c__Name_ee85a0da');
+    };
+
+    get inputEmailAddress() { 
+        return $('#c__Email_ee85a0da');
+    };
+
+    addAPurchase(datePurchased, statePurchased) {  
+        $("//button[normalize-space()='Add A Purchase']").click();
+        $("//input[@type='date']").click();
+        $("//input[@type='date']").setValue(datePurchased);
+        $('div > .null-value').click();
+        $(`//li[normalize-space()='${statePurchased}']`).click();
+    };
+
+    get attestCheckbox() {
+        return $("//span[@tabindex='0']");
+    };
+
+    get inputSignature() {
+        return $('#c__SignatureText_ee85a0da');
+    }
+
+    get submitButton() {
+        return $("//button[normalize-space()='Submit']");
+    };
+
+    get sucessfulSubmission() {
+        return $('#c__formAppee85a0da');
+    };
+
+    get unsucessfulSubmission() {
+        return $("#c__Email_ee85a0da_error");   
+    };
+
+    async submitMandatoryFields(name, emailAddress, datePurchased, statePurchased, signature) {
+        await this.inputName.setValue(name);
+        await this.inputEmailAddress.setValue(emailAddress);
+        await this.addAPurchase(datePurchased, statePurchased);
+        await this.attestCheckbox.click();
+        await this.inputSignature.setValue(signature);
+        await browser.pause(1000);
+        await this.submitButton.click();
+    }
+}
+
+export default new FileAClaimPage();
---
+++ test/specs/fileAClaim.spec.js
@@ -0,0 +1,21 @@
+import FileAClaimPage from "../pageObjects/fileAClaim.page";
+
+describe('Test - Contact Us Form', () => {
+    beforeEach(async() => {
+        await FileAClaimPage.open(); 
+    });
+
+    it('Sucessful Submission - All mandatory fields are valid.', async() => {
+        FileAClaimPage.submitMandatoryFields("Alecs", "alecs@test.com", "07/18/2022", "NJ", "Alecsander Camilo");  
+    
+        await expect(FileAClaimPage.sucessfulSubmission).toHaveText("Your claim has been submitted.")
+    });
+
+       
+
+    it('Unsucessful Submission - Email Address not valid.', async() => {
+        FileAClaimPage.submitMandatoryFields("Alecs", "", "07/18/2022", "", "Alecsander Camilo");
+        
+        await expect(FileAClaimPage.unsucessfulSubmission).toHaveText("*required");
+    });
+});
